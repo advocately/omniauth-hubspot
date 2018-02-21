9a03cbaed21b940d6e912844e0381de7d56c62b1
Alex Willemsma <alexw@smile.io>
2018-02-21 11:24:51 -0500

    Fix hubspot strategy to work with latest omniauth-oauth2 gem.

--- lib/omniauth/strategies/hubspot.rb
+++ lib/omniauth/strategies/hubspot.rb
@@ -12,6 +12,10 @@ module OmniAuth
         authorize_url: 'https://app.hubspot.com/oauth/authorize',
         token_url: 'oauth/v1/token'
       }
+
+      def callback_url
+        full_host + script_name + callback_path
+      end
     end
   end
 end
